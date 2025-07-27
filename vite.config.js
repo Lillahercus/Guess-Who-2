8161836185f78f5e22966a39bc3b76a251726215
Lillahercus <kurtyanlilla06@gmail.com>
2025-07-27 12:28:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // ide nem szükséges explicit external hozzáadás, hacsak nem akarsz valamit kizárni
+    }
+  },
+  base: './',  // Fontos, hogy relatív base legyen buildhez
 })
